059a4c301e4e8dc8c554820abc0ffde1f20b65f3
wahidurcs <wahidcs24@gmail.com>
2024-10-23 00:46:39 +0600

    Add solution for Two Sum problem

---
+++ Array/twoSum.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums, target):
+        n = len(nums)
+        for i in range(n):
+            for j in range(i + 1, n):
+                if nums[i] + nums[j] == target:
+                    return [i, j] ##return indices two numbers
+
+        return [] ##if no pair found return empty
+
