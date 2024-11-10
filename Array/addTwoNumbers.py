aaa4b878a0c98533a2d9e8d86f98848951417381
wahidurcs <wahidcs24@gmail.com>
2024-11-10 22:31:25 +0600

    Update addTwoNumbers.py

--- Array/addTwoNumbers.py
+++ Array/addTwoNumbers.py
@@ -1,1 +1,21 @@
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        dummy = ListNode(0)
+        current = dummy
+        carry = 0
 
+        while l1 is not None or l2 is not None:
+            x = l1.val if l1 is not None else 0
+            y = l2.val if l2 is not None else 0
+            sum = x + y+carry
+            carry = sum // 10
+            current.next = ListNode(sum % 10)
+            current = current.next
+
+            if l1 is not None: l1 = l1.next
+            if l2 is not None: l2 = l2.next
+
+        if carry > 0:
+            current.next = ListNode(carry)
+
+        return dummy.next
